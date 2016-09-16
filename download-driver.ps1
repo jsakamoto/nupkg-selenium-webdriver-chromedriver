39985b69bac4c6742443f770b0fd35f559837fb8
jsakamoto <junichi.jp@gmail.com>
2016-09-16 11:29:03 +0900

    Update build script for v.2.24 release.

--- download-driver.ps1
+++ download-driver.ps1
@@ -1,5 +1,5 @@
 # constants
-$version = "2.23"
+$version = "2.24"
 $driverName = "chromedriver.exe"
 $zipName = "chromedriver_win32.$version.zip"
 $downloadUrl = "https://chromedriver.storage.googleapis.com/$version/chromedriver_win32.zip"
