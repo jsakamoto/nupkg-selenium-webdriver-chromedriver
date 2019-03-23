2ef8a59dd4c0de570b07278d8ba80121e0280165
jsakamoto <junichi.jp@gmail.com>
2019-03-22 17:38:39 -0700

    Add test drive program.

---
+++ TestDrives/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TestDrive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var driver = new OpenQA.Selenium.Chrome.ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://www.bing.com/");
+                driver.FindElementById("sb_form_q").SendKeys("Selenium WebDriver");
+                driver.FindElementById("sb_form_go").Click();
+
+                Console.WriteLine("OK");
+                Console.ReadKey(intercept: true);
+            }
+        }
+    }
+}
