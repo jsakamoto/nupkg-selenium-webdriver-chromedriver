84151aad187c2feeeba31504fa987e34ba3c778a
jsakamoto <junichi.jp@gmail.com>
2019-08-12 23:11:52 +0900

    Add unit test for build script by dotnet CLI building

---
+++ test/Lib/Shell.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Selenium.WebDriver.ChromeDriver.NuPkg.Test.Lib
+{
+    public static class Shell
+    {
+        public static int ErrorLevel { get; set; }
+
+        public static bool Exists(string dir, string wildCard)
+        {
+            return Directory.GetFiles(dir, wildCard, SearchOption.TopDirectoryOnly).Any();
+        }
+
+        public static void DeleteDir(string dir)
+        {
+            if (Directory.Exists(dir)) Directory.Delete(dir, recursive: true);
+        }
+
+        public static void XcopyDir(string srcDir, string dstDir)
+        {
+            Directory.CreateDirectory(dstDir);
+
+            var srcFileNames = Directory.GetFiles(srcDir);
+            foreach (var srcFileName in srcFileNames)
+            {
+                var dstFileName = Path.Combine(dstDir, Path.GetFileName(srcFileName));
+                File.Copy(srcFileName, dstFileName);
+            }
+
+            var srcSubDirs = Directory.GetDirectories(srcDir);
+            foreach (var srcSubDir in srcSubDirs)
+            {
+                var dstSubDir = Path.Combine(dstDir, Path.GetFileName(srcSubDir));
+                XcopyDir(srcSubDir, dstSubDir);
+            }
+        }
+
+        public static int Run(string workDir, params string[] args)
+        {
+            var pi = new ProcessStartInfo
+            {
+                WorkingDirectory = workDir,
+                FileName = args.First(),
+                Arguments = string.Join(" ", args.Skip(1)),
+                UseShellExecute = false,
+            };
+            var process = Process.Start(pi);
+            process.WaitForExit();
+            ErrorLevel = process.ExitCode;
+            return process.ExitCode;
+        }
+    }
+}
