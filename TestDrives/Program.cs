fb07c9b87cee9de45df57196481401eff74dc28c
jsakamoto <junichi.jp@gmail.com>
2020-02-05 19:45:26 +0900

    Improve test drive program.

--- TestDrives/Program.cs
+++ TestDrives/Program.cs
@@ -8,9 +8,10 @@ namespace TestDrive
         {
             using (var driver = new OpenQA.Selenium.Chrome.ChromeDriver())
             {
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
                 driver.Navigate().GoToUrl("https://www.bing.com/");
                 driver.FindElementById("sb_form_q").SendKeys("Selenium WebDriver");
-                driver.FindElementById("sb_form_go").Click();
+                driver.FindElementByClassName("search").Click();
 
                 Console.WriteLine("OK");
                 Console.ReadKey(intercept: true);
