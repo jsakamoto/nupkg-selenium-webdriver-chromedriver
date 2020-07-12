66da58a48adf55c0fcb870e9486f9a104f28dbd0
jsakamoto <junichi.jp@gmail.com>
2020-07-12 14:00:55 +0900

    Integrate preprocessor script with other webdriver packages

--- buildTools/preprocess-targets-file.ps1
+++ buildTools/preprocess-targets-file.ps1
@@ -1,20 +1,21 @@
-$propFile = Get-ChildItem .\src\DefineProperties*.targets
-$propContents = Get-Content $propFile
+$targetsSrcFiles = Get-ChildItem .\src\*.targets.src
 
-$targetsSrcFile = Get-ChildItem .\src\*.targets.src
-$targetsSrcContents = Get-Content $targetsSrcFile
-
-$targetsOutFilePath = $targetsSrcFile.FullName -replace "\.src$", ""
-
-if (Test-Path $targetsOutFilePath) { Remove-Item $targetsOutFilePath }
-
-foreach ($line in $targetsSrcContents) {
-    if ($line.Trim() -eq "<!-- insert DefineProperties.targets -->") {
-        foreach ($propLine in $propContents[1..($propContents.Count - 2)]) {
-            Write-Output "  $propLine".TrimEnd() | Out-File $targetsOutFilePath -Append -Encoding utf8
+foreach ($targetsSrcFile in $targetsSrcFiles) {
+    
+    $targetsOutFilePath = $targetsSrcFile.FullName -replace "\.src$", ""
+    if (Test-Path $targetsOutFilePath) { Remove-Item $targetsOutFilePath }
+    
+    $targetsSrcContents = Get-Content $targetsSrcFile
+    foreach ($line in $targetsSrcContents) {
+        if ($line.Trim() -match "<!-- insert ([^ ]+.targets) -->") {
+            $propFile = Join-Path $targetsSrcFile.DirectoryName $Matches[1]
+            $propContents = Get-Content $propFile
+            foreach ($propLine in $propContents[1..($propContents.Count - 2)]) {
+                Write-Output "  $propLine".TrimEnd() | Out-File $targetsOutFilePath -Append -Encoding utf8
+            }
         }
-    }
-    else {
-        Write-Output $line | Out-File $targetsOutFilePath -Append -Encoding utf8
+        else {
+            Write-Output $line | Out-File $targetsOutFilePath -Append -Encoding utf8
+        }
     }
 }
