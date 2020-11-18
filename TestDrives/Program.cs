2a50049f74efec93f6addbfb29bebf3df0bffc48
jsakamoto <junichi.jp@gmail.com>
2020-11-19 08:07:50 +0900

    Change test drive project to .NET 5

--- TestDrives/Program.cs
+++ TestDrives/Program.cs
@@ -1,21 +1,11 @@
 ﻿using System;
 
-namespace TestDrive
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            using (var driver = new OpenQA.Selenium.Chrome.ChromeDriver())
-            {
-                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
-                driver.Navigate().GoToUrl("https://www.bing.com/");
-                driver.FindElementById("sb_form_q").SendKeys("Selenium WebDriver");
-                driver.FindElementByClassName("search").Click();
+using var driver = new OpenQA.Selenium.Chrome.ChromeDriver();
 
-                Console.WriteLine("OK");
-                Console.ReadKey(intercept: true);
-            }
-        }
-    }
-}
+driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+driver.Navigate().GoToUrl("https://www.bing.com/");
+driver.FindElementById("sb_form_q").SendKeys("Selenium WebDriver");
+driver.FindElementByClassName("search").Click();
+
+Console.WriteLine("OK");
+Console.ReadKey(intercept: true);
