996f84c1c35cbd640ab92c7aa07ab13293281fdb
jsakamoto <junichi.jp@gmail.com>
2020-12-04 20:14:05 +0900

    Change unit test framework to NUnit.

--- test/BuildProjectABTest.cs
+++ test/BuildProjectABTest.cs
@@ -1,39 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
+using NUnit.Framework;
 using Selenium.WebDriver.ChromeDriver.NuPkg.Test.Lib;
-using Xunit;
 
 namespace Selenium.WebDriver.ChromeDriver.NuPkg.Test
 {
-    public class BuildProjectABTest : IDisposable
+    [Parallelizable(ParallelScope.All)]
+    public class BuildProjectABTest
     {
-        private string WorkDir { get; }
-
-        public BuildProjectABTest()
-        {
-            WorkDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Guid.NewGuid().ToString("N"));
-            var srcDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ProjectAB");
-            Shell.XcopyDir(srcDir, WorkDir);
-        }
-
-        public void Dispose()
-        {
-            Shell.DeleteDir(WorkDir);
-        }
-
-        [Fact]
+        [Test]
         public void Output_of_ProjectB_Contains_DriverFile_Test()
         {
+            using var workSpace = new WorkSpace(copyFrom: "ProjectAB");
+
             //var devenv = Environment.ExpandEnvironmentVariables(Path.Combine("%DevEnvDir%", "devenv.exe"));
             var devenv = @"C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\Common7\IDE\devenv.exe";
-            Shell.Run(WorkDir, "nuget", "restore").Is(0);
-            Shell.Run(WorkDir, devenv, "ProjectAB.sln", "/Build").Is(0);
+            Shell.Run(workSpace, "nuget", "restore").Is(0);
+            Shell.Run(workSpace, devenv, "ProjectAB.sln", "/Build").Is(0);
 
-            var outDir = Path.Combine(WorkDir, "ProjectB", "bin", "Debug", "net472");
+            var outDir = Path.Combine(workSpace, "ProjectB", "bin", "Debug", "net472");
             var driverFullPath1 = Path.Combine(outDir, "chromedriver");
             var driverFullPath2 = Path.Combine(outDir, "chromedriver.exe");
             (File.Exists(driverFullPath1) || File.Exists(driverFullPath2)).IsTrue();
