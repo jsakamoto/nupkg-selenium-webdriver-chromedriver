4db58fe2e48a0255fa1165dbfbee7a965b2dc0c1
jsakamoto <junichi.jp@gmail.com>
2024-04-24 21:53:53 +0900

    Modernize the unite tests

--- test/Project/program.cs
+++ test/Project/program.cs
@@ -1,11 +1,13 @@
 ﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
 
-using var driver = new ChromeDriver(AppDomain.CurrentDomain.BaseDirectory);
-        
+using var driver = new OpenQA.Selenium.Chrome.ChromeDriver(AppDomain.CurrentDomain.BaseDirectory);
+
+driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
 driver.Navigate().GoToUrl("https://www.bing.com/");
+await Task.Delay(1000);
+
 driver.FindElement(By.Id("sb_form_q")).SendKeys("Selenium WebDriver");
-driver.FindElement(By.Id("sb_form_go")).Click();
+driver.FindElement(By.Id("sb_form_q")).SendKeys(Keys.Enter);
 
 Console.WriteLine("OK");
 Console.ReadKey(intercept: true);
