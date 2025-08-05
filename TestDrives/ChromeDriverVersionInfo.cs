a394b37a721bbb6dcc3de49429127930be712096
jsakamoto <junichi.jp@gmail.com>
2025-08-06 08:06:19 +0900

    Package version 139.0.7258.66 official release

--- TestDrives/ChromeDriverVersionInfo.cs
+++ TestDrives/ChromeDriverVersionInfo.cs
@@ -2,5 +2,5 @@ namespace TestDrive;
 
 internal static class ChromeDriverVersionInfo
 {
-    internal const string VersionText = "139.0.7258.6600-beta";
+    internal const string VersionText = "139.0.7258.6600";
 }
