cba4b4c389853cabb702036707cc92b7dbf1e2d0
jsakamoto <junichi.jp@gmail.com>
2025-08-13 08:06:37 +0900

    Update ChromeDriver to version 139.0.7258.68

--- TestDrives/ChromeDriverVersionInfo.cs
+++ TestDrives/ChromeDriverVersionInfo.cs
@@ -1,6 +1,5 @@
 namespace TestDrive;
-
 internal static class ChromeDriverVersionInfo
 {
-    internal const string VersionText = "139.0.7258.6600";
+    internal const string VersionText = "139.0.7258.6800";
 }
