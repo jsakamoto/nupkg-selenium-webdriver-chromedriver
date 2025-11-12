3f918357c24fa5367f7b4c8e2dd55caf31117775
jsakamoto <junichi.jp@gmail.com>
2025-11-13 07:37:03 +0900

    Update ChromeDriver to version 142.0.7444.162

--- TestDrives/ChromeDriverVersionInfo.cs
+++ TestDrives/ChromeDriverVersionInfo.cs
@@ -2,5 +2,5 @@ namespace TestDrive;
 
 internal static class ChromeDriverVersionInfo
 {
-    internal const string VersionText = "142.0.7444.6100";
+    internal const string VersionText = "142.0.7444.16200";
 }
