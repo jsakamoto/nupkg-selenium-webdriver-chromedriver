ff2ee7993788793514601d1faf23bda1b08c9a58
jsakamoto <junichi.jp@gmail.com>
2025-12-11 07:25:41 +0900

    Update ChromeDriver to version 144.0.7559.20

--- TestDrives/ChromeDriverVersionInfo.cs
+++ TestDrives/ChromeDriverVersionInfo.cs
@@ -1,5 +1,6 @@
 namespace TestDrive;
+
 internal static class ChromeDriverVersionInfo
 {
-    internal const string VersionText = "144.0.7559.300-beta";
+    internal const string VersionText = "144.0.7559.2000-beta";
 }
