805a0b28b504837a88de82f9e4f70bbf23f98c02
jsakamoto <junichi.jp@gmail.com>
2026-04-18 10:38:35 +0900

    Update ChromeDriver to version 148.0.7778.40
    # Conflicts:
    #	test/ProjectAB/ProjectA/packages.config

--- TestDrives/ChromeDriverVersionInfo.cs
+++ TestDrives/ChromeDriverVersionInfo.cs
@@ -1,6 +1,6 @@
-namespace TestDrive;
+﻿namespace TestDrive;
 
 internal static class ChromeDriverVersionInfo
 {
-    internal const string VersionText = "148.0.7778.500-beta";
+    internal const string VersionText = "148.0.7778.4000-beta";
 }
