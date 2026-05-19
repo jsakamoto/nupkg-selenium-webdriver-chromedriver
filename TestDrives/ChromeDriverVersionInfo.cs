273c4798e07695e4b2781965ff91ff646771e020
jsakamoto <junichi.jp@gmail.com>
2026-05-20 07:52:02 +0900

    Update ChromeDriver to version 148.0.7778.178

--- TestDrives/ChromeDriverVersionInfo.cs
+++ TestDrives/ChromeDriverVersionInfo.cs
@@ -1,6 +1,6 @@
-namespace TestDrive;
+﻿namespace TestDrive;
 
 internal static class ChromeDriverVersionInfo
 {
-    internal const string VersionText = "148.0.7778.16700";
+    internal const string VersionText = "148.0.7778.17800";
 }
