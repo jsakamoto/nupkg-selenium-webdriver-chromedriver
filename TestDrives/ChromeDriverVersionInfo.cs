bd36100100626e321d05058e0e94a451c28bd34e
jsakamoto <junichi.jp@gmail.com>
2026-08-20 08:06:39 +0900

    Update ChromeDriver to version 152.0.7977.54

--- TestDrives/ChromeDriverVersionInfo.cs
+++ TestDrives/ChromeDriverVersionInfo.cs
@@ -2,5 +2,5 @@
 
 internal static class ChromeDriverVersionInfo
 {
-    internal const string VersionText = "152.0.7977.4200-beta";
+    internal const string VersionText = "152.0.7977.5400-beta";
 }
