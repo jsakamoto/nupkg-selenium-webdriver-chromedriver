831ddbaa977ee2d42a84fca764bdd470dd9a9fc9
jsakamoto <junichi.jp@gmail.com>
2026-09-09 07:58:58 +0900

    Update ChromeDriver to version 153.0.8010.36

--- TestDrives/ChromeDriverVersionInfo.cs
+++ TestDrives/ChromeDriverVersionInfo.cs
@@ -2,5 +2,5 @@ namespace TestDrive;
 
 internal static class ChromeDriverVersionInfo
 {
-    internal const string VersionText = "153.0.8010.1200-beta";
+    internal const string VersionText = "153.0.8010.3600";
 }
